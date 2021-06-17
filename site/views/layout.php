61cd0cac469459e1e27b844ad94479421512d3a3
phongnt27 <ntp27052000@gmail.com>
2021-06-17 23:00:42 +0700

    Update layout.php

--- site/views/layout.php
+++ site/views/layout.php
@@ -61,30 +61,33 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12 col-md-6">
-                    <h6>About</h6>
-                    <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+                    <h6>Về chúng tôi</h6>
+                    <p class="text-justify">DAWN.com
+                        là một WEBSITE chia sẽ kiến thức lập trình. 
+                        DAWN tập trung vào việc cung cấp kiến hiệu quả nhất và giúp thự hiện chúng đơn giản. 
+                        Chúng tôi sẽ giúp các lập trình viên xây dựng các khái niệm bằng các ngôn ngữ lập trình khác nhau bao gồm 
+                        C, C ++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL và Algorithm.
+                     </p>
                 </div>
 
                 <div class="col-xs-6 col-md-3">
-                    <h6>Categories</h6>
+                    <h6>Danh mục</h6>
                     <ul class="footer-links">
-                    <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
-                    <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
-                    <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
-                    <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
-                    <li><a href="http://scanfcode.com/category/android/">Android</a></li>
-                    <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+                    <li><a href="">C</a></li>
+                    <li><a href="">UI Design</a></li>
+                    <li><a href="">PHP</a></li>
+                    <li><a href="">Java</a></li>
+                    <li><a href="">Android</a></li>
+                    <li><a href="">Templates</a></li>
                     </ul>
                 </div>
 
                 <div class="col-xs-6 col-md-3">
-                    <h6>Quick Links</h6>
+                    <h6>Các links liên kết</h6>
                     <ul class="footer-links">
-                    <li><a href="http://scanfcode.com/about/">About Us</a></li>
-                    <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
-                    <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
-                    <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
-                    <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+                    <li><a href="http://scanfcode.com/about/">Về chúng tôi</a></li>
+                    <li><a href="http://scanfcode.com/contact/">Liên hệ với chúng tô</a></li>
+                    <li><a href="http://scanfcode.com/privacy-policy/">Chính sách bảo mật</a></li>
                     </ul>
                 </div>
             </div>
@@ -94,7 +97,7 @@
             <div class="row">
                 <div class="col-md-8 col-sm-6 col-xs-12">
                     <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
-                <a href="#">Scanfcode</a>.
+                <a href="#">DAWN</a>.
                     </p>
                 </div>
 
